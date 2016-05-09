d2789f9757332f451ea3a8fcea7f74eea92c72e5
jeremyhilliard14 <jeremyhilliard14@gmail.com>
2016-05-09 10:58:28 -0400

    Added resume and link

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var servStatic = require('serve-static');
-connect().use(servStatic(__dirname)).listen(8000, function(){
-	console.log('Listening on port 8000...');
+connect().use(servStatic(__dirname)).listen(8001, function(){
+	console.log('Listening on port 8001...');
 });
